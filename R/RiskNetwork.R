77f7ab6c491a9be6ed5a1a4e08591fbdafb99de0
paulgovan <pgovan1@aggienetwork.com>
2016-07-15 16:11:56 -0400

    Updated package to remove notes

--- R/RiskNetwork.R
+++ R/RiskNetwork.R
@@ -2,6 +2,11 @@
 #'
 #' RiskNetwork is a Shiny web application for risk network modeling and
 #' analysis.
+#' @import bnlearn
+#' @import d3heatmap
+#' @import lattice
+#' @import networkD3
+#' @import rhandsontable
 #' @import shiny
 #' @import shinydashboard
 #' @export
